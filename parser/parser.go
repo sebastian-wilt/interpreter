b283a01a80522f9f7a40d29f0e09d1dcdc885fe9
Sebastian Wilt <wilt.sebastian@gmail.com>
2025-04-21 22:26:27 +0200

    Implement parsing of expressions

---
+++ parser/parser.go
@@ -0,0 +1,335 @@
+package parser
+
+import (
+	"errors"
+	"fmt"
+	"interpreter/ast"
+	"interpreter/token"
+	"slices"
+)
+
+/*
+	Recursive descent parser
+
+	Operator precedence:
+	expression ::= assignment;
+	assignment ::= IDENTIFIER "=" assignment | equality;
+	equality ::= comparison ( ( "!=" | "==") comparison)*;
+	comparison ::= term ( ( ">" | ">=" | "<=" | "<") term)*;
+	term ::= factor ( ( "-" | "+" ) factor)*;
+	factor ::= unary ( ( "/" | "*" | "%") unary)*;
+	unary ::= ("!" | "-") unary | exponent;
+	exponent ::= primary ("**") primary | primary;
+	primary ::=  IDENTIFIER | INTEGER | REAL | STRING | "true" | "false" | "(" expression ")";
+*/
+
+type Parser struct {
+	tokens  []token.Token // List of tokens to parse
+	current int           // Current token
+	errors  []error       // Parse errors
+}
+
+func NewParser(tokens []token.Token) Parser {
+	return Parser{
+		tokens:  tokens,
+		current: 0,
+		errors:  []error{},
+	}
+}
+
+// Parse entire input
+// Returns root node of AST
+func (p *Parser) Parse() (ast.Expr, []error) {
+	root, _ := p.expression()
+	return root, p.errors
+}
+
+// Checks if next token is one of tokens
+// Advances if found
+func (p *Parser) expect(tokens []token.TokenType) bool {
+	if slices.Contains(tokens, p.tokens[p.current].Kind) {
+		p.advance()
+		return true
+	}
+
+	return false
+}
+
+// Check if next token is kind
+func (p *Parser) check(kind token.TokenType) bool {
+	if p.is_at_end() {
+		return false
+	}
+	return kind == p.tokens[p.current].Kind
+}
+
+// Advance parser one token if kind is next token
+// Returns error without advancing if not found
+func (p *Parser) consume(kind token.TokenType) (token.Token, error) {
+	if p.check(kind) {
+		return p.advance(), nil
+	}
+
+	tok := p.peek()
+	return token.Token{}, p.error(fmt.Sprintf("Unexpected token at line: %d. Expected %v, found %v", tok.Pos.Row, kind, tok.Kind))
+}
+
+func (p *Parser) is_at_end() bool {
+	return p.current == len(p.tokens)
+}
+
+// Create error with message and add to list of errors
+func (p *Parser) error(message string) error {
+	err := errors.New(message)
+	p.errors = append(p.errors, err)
+	return err
+}
+
+// Advance one token
+func (p *Parser) advance() token.Token {
+	if !p.is_at_end() {
+		p.current += 1
+	}
+	return p.previous()
+}
+
+// Peek at next token without advancing
+// Returns last token if at end
+func (p *Parser) peek() token.Token {
+	if p.is_at_end() {
+		return p.previous()
+	}
+
+	return p.tokens[p.current]
+}
+
+// Return previous token
+func (p *Parser) previous() token.Token {
+	return p.tokens[p.current-1]
+}
+
+/*
+Precedence:
+
+	expression ::= assignment;
+	assignment ::= IDENTIFIER "=" assignment | equality;
+	equality ::= comparison ( ( "!=" | "==") comparison)*;
+	comparison ::= term ( ( ">" | ">=" | "<=" | "<") term)*;
+	term ::= factor ( ( "-" | "+" ) factor)*;
+	factor ::= unary ( ( "/" | "*" | "%") unary)*;
+	unary ::= ("!" | "-") unary | exponent;
+	exponent ::= primary ("**") primary | primary;
+	primary ::=  IDENTIFIER | INTEGER | REAL | STRING | "true" | "false" | "(" expression ")";
+*/
+
+// Parse expression
+func (p *Parser) expression() (ast.Expr, error) {
+	return p.assignment()
+}
+
+// Parse expressions with same precedence as assignments
+func (p *Parser) assignment() (ast.Expr, error) {
+	equality, err := p.equality()
+	if err != nil {
+		return nil, err
+	}
+
+	if p.expect([]token.TokenType{token.EQUAL}) {
+		value, err := p.assignment()
+		if err != nil {
+			return nil, err
+		}
+
+		if ident, ok := equality.(*ast.Ident); ok {
+			return &ast.AssignmentExpr{
+				Pos:   ident.Position(),
+				Name:  *ident,
+				Value: value,
+			}, nil
+		}
+
+		return &ast.AssignmentExpr{}, p.error(fmt.Sprintf("Invalid assignment target at line %d.", equality.Position().Row))
+	}
+
+	return equality, nil
+}
+
+// Parse expressions with same precedence as equality.
+func (p *Parser) equality() (ast.Expr, error) {
+	comparison, err := p.comparison()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.expect([]token.TokenType{token.BANG_EQUAL, token.EQUAL_EQUAL}) {
+		op := p.previous()
+		right, err := p.comparison()
+		if err != nil {
+			return nil, err
+		}
+
+		comparison = &ast.BinaryExpr{
+			Left:  comparison,
+			Op:    op,
+			Pos:   op.Pos,
+			Right: right,
+		}
+	}
+
+	return comparison, nil
+}
+
+// Parse expressions with same precedence as comparisons
+func (p *Parser) comparison() (ast.Expr, error) {
+	term, err := p.term()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.expect([]token.TokenType{token.GREATER, token.GREATER_EQUAL, token.LESS_EQUAL, token.LESS}) {
+		op := p.previous()
+		right, err := p.term()
+		if err != nil {
+			return nil, err
+		}
+
+		term = &ast.BinaryExpr{
+			Left:  term,
+			Op:    op,
+			Pos:   op.Pos,
+			Right: right,
+		}
+	}
+
+	return term, nil
+}
+
+// Parse binary PLUS and MINUS
+func (p *Parser) term() (ast.Expr, error) {
+	factor, err := p.factor()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.expect([]token.TokenType{token.MINUS, token.PLUS}) {
+		op := p.previous()
+		right, err := p.factor()
+		if err != nil {
+			return nil, err
+		}
+
+		factor = &ast.BinaryExpr{
+			Left:  factor,
+			Op:    op,
+			Pos:   op.Pos,
+			Right: right,
+		}
+	}
+
+	return factor, nil
+}
+
+// Parse binary division, multiplication and modulo
+func (p *Parser) factor() (ast.Expr, error) {
+	unary, err := p.unary()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.expect([]token.TokenType{token.SLASH, token.STAR, token.PERCENT}) {
+		op := p.previous()
+		right, err := p.unary()
+		if err != nil {
+			return nil, err
+		}
+
+		unary = &ast.BinaryExpr{
+			Left:  unary,
+			Op:    op,
+			Pos:   op.Pos,
+			Right: right,
+		}
+	}
+
+	return unary, nil
+}
+
+// Parse unary expressions
+func (p *Parser) unary() (ast.Expr, error) {
+	if p.expect([]token.TokenType{token.BANG, token.MINUS}) {
+		op := p.previous()
+		unary, err := p.unary()
+		if err != nil {
+			return nil, err
+		}
+
+		return &ast.UnaryExpr{
+			Pos:  op.Pos,
+			Op:   op,
+			Expr: unary,
+		}, nil
+	}
+
+	return p.exponent()
+}
+
+// Parse exponent expressions
+func (p *Parser) exponent() (ast.Expr, error) {
+	primary, err := p.primary()
+	if err != nil {
+		return nil, err
+	}
+
+	if p.expect([]token.TokenType{token.STAR_STAR}) {
+		op := p.previous()
+		right, err := p.primary()
+		if err != nil {
+			return nil, err
+		}
+
+		return &ast.BinaryExpr{
+			Left:  primary,
+			Op:    op,
+			Pos:   op.Pos,
+			Right: right,
+		}, nil
+	}
+
+	return primary, nil
+}
+
+// Parse literals and groupings
+func (p *Parser) primary() (ast.Expr, error) {
+	if p.expect([]token.TokenType{token.LEFT_PAREN}) {
+		lparen := p.previous()
+
+		expr, err := p.expression()
+		if err != nil {
+			return nil, err
+		}
+
+		// Expect ')' after expression
+		_, err = p.consume(token.RIGHT_PAREN)
+		if err != nil {
+			return nil, err
+		}
+
+		return &ast.GroupingExpr{
+			Pos:  lparen.Pos,
+			Expr: expr,
+		}, nil
+	}
+
+	literals := []token.TokenType{token.IDENT, token.INTEGER, token.REAL, token.STRING, token.TRUE, token.FALSE}
+	if p.expect(literals) {
+		token := p.previous()
+
+		return &ast.LiteralExpr{
+			Pos:   token.Pos,
+			Kind:  token.Kind,
+			Value: token.Value,
+		}, nil
+	}
+
+	return nil, p.error(fmt.Sprintf("Expected expression at line %d", p.peek().Pos.Row))
+}
---
+++ parser/parser_test.go
@@ -0,0 +1,131 @@
+package parser
+
+import (
+	"interpreter/ast"
+	"interpreter/lexer"
+	"interpreter/token"
+	"testing"
+)
+
+func TestUnaryExpression(t *testing.T) {
+	input := "-5"
+
+	lexer := lexer.NewLexer([]byte(input))
+	tokens, errors := lexer.Tokenize()
+	if len(errors) != 0 {
+		t.Log("Expected no lexer errors")
+
+		for i, err := range errors {
+			t.Logf("Error %d: %v", i, err)
+		}
+
+		t.FailNow()
+	}
+
+	parser := NewParser(tokens)
+	expr, err := parser.expression()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	unary := verifyExprType[*ast.UnaryExpr](t, expr)
+	verifyOperator(t, unary.Op, token.Token{Kind: token.MINUS})
+
+	literal := verifyExprType[*ast.LiteralExpr](t, unary.Expr)
+	verifyLiteral(t, literal, ast.LiteralExpr{Kind: token.INTEGER, Value: "5"})
+}
+
+func TestBinaryExpression(t *testing.T) {
+	input := "10 * 25"
+
+	lexer := lexer.NewLexer([]byte(input))
+	tokens, errors := lexer.Tokenize()
+	if len(errors) != 0 {
+		t.Log("Expected no lexer errors")
+
+		for i, err := range errors {
+			t.Logf("Error %d: %v", i, err)
+		}
+
+		t.FailNow()
+	}
+
+	parser := NewParser(tokens)
+	expr, err := parser.expression()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	binary := verifyExprType[*ast.BinaryExpr](t, expr)
+	verifyOperator(t, binary.Op, token.Token{Kind: token.STAR})
+
+	left := verifyExprType[*ast.LiteralExpr](t, binary.Left)
+	verifyLiteral(t, left, ast.LiteralExpr{Kind: token.INTEGER, Value: "10"})
+
+	right := verifyExprType[*ast.LiteralExpr](t, binary.Right)
+	verifyLiteral(t, right, ast.LiteralExpr{Kind: token.INTEGER, Value: "25"})
+}
+
+func TestNestedExpression(t *testing.T) {
+	input := "1 + 2 + 3"
+
+	lexer := lexer.NewLexer([]byte(input))
+	tokens, errors := lexer.Tokenize()
+	if len(errors) != 0 {
+		t.Log("Expected no lexer errors")
+
+		for i, err := range errors {
+			t.Logf("Error %d: %v", i, err)
+		}
+
+		t.FailNow()
+	}
+
+	parser := NewParser(tokens)
+	expr, err := parser.expression()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	binary := verifyExprType[*ast.BinaryExpr](t, expr)
+	verifyOperator(t, binary.Op, token.Token{Kind: token.PLUS})
+
+	left := verifyExprType[*ast.BinaryExpr](t, binary.Left)
+	verifyOperator(t, left.Op, token.Token{Kind: token.PLUS})
+
+	lleft := verifyExprType[*ast.LiteralExpr](t, left.Left)
+	verifyLiteral(t, lleft, ast.LiteralExpr{Kind: token.INTEGER, Value: "1"})
+
+	lright := verifyExprType[*ast.LiteralExpr](t, left.Right)
+	verifyLiteral(t, lright, ast.LiteralExpr{Kind: token.INTEGER, Value: "2"})
+
+	right := verifyExprType[*ast.LiteralExpr](t, binary.Right)
+	verifyLiteral(t, right, ast.LiteralExpr{Kind: token.INTEGER, Value: "3"})
+}
+
+func verifyExprType[T ast.Expr](t *testing.T, expr ast.Expr) T {
+	var expected T
+	node, ok := expr.(T)
+
+	if !ok {
+		t.Fatalf("Unexpected expression type. Expected %T, found %T", expected, expr)
+	}
+
+	return node
+}
+
+func verifyLiteral(t *testing.T, literal *ast.LiteralExpr, expected ast.LiteralExpr) {
+	if literal.Kind != expected.Kind {
+		t.Fatalf("Unexpected literal type. Expected %v, found %v", expected.Kind, literal.Kind)
+	}
+
+	if literal.Value != expected.Value {
+		t.Fatalf("Unexpected literal value. Expected %q, found %q", expected.Value, literal.Value)
+	}
+}
+
+func verifyOperator(t *testing.T, operator token.Token, expected token.Token) {
+	if operator.Kind != expected.Kind {
+		t.Fatalf("Unexpected operator type. Expected %v, found %v", expected.Kind, operator.Kind)
+	}
+}
